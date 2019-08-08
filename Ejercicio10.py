aefd7c8abd81361907eba50ab7650660eece2b5a
Cristopher Duarte <cristopherduarte50@gmail.com>
2019-08-08 18:00:24 -0500

    8/08/2019 6:00 pm

---
+++ Ejercicio10.py
@@ -0,0 +1,8 @@
+directorio={
+    "Nombre":["Cristopher","Alejandro"],
+    "Apellido": ["Duarte Pimienta","Gutierrez Pimienta"],
+    "Edad":[18,17]
+    }
+for x in directorio:
+    for y in range(2):
+      print (x, ":", directorio[x][y])
